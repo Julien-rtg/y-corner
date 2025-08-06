5fb757560680ab065d06eb4e68341e73b4fc2ac8
julien <julien.rittl@gmail.com>
2025-08-06 18:49:34 +0200

    integration test mongodb

--- backend/tests/Integration/ChatMongoDBIntegrationTest.php
+++ backend/tests/Integration/ChatMongoDBIntegrationTest.php
@@ -18,6 +18,7 @@ class ChatMongoDBIntegrationTest extends KernelTestCase
     private TestConnection $connection;
     private string $testUserId1 = 'test-user-1';
     private string $testUserId2 = 'test-user-2';
+    private string $testUserId3 = 'test-user-3';
     
     protected function setUp(): void
     {
@@ -145,5 +146,131 @@ class ChatMongoDBIntegrationTest extends KernelTestCase
         
         $this->assertNotNull($updatedMessage, 'Message not found in database');
         $this->assertTrue($updatedMessage->isSeen(), 'Message was not marked as seen in the database');
+    }
+    
+    public function testGetMessagesBetweenUsers(): void
+    {
+        $message1 = new ChatMessage();
+        $message1->setFromUserId($this->testUserId1)
+            ->setToUserId($this->testUserId2)
+            ->setMessage('Message 1 from user1 to user2')
+            ->setSeen(false);
+        
+        $message2 = new ChatMessage();
+        $message2->setFromUserId($this->testUserId2)
+            ->setToUserId($this->testUserId1)
+            ->setMessage('Message 2 from user2 to user1')
+            ->setSeen(false);
+            
+        $message3 = new ChatMessage();
+        $message3->setFromUserId($this->testUserId1)
+            ->setToUserId($this->testUserId3)
+            ->setMessage('Message 3 from user1 to user3')
+            ->setSeen(false);
+        
+        $this->documentManager->persist($message1);
+        $this->documentManager->persist($message2);
+        $this->documentManager->persist($message3);
+        $this->documentManager->flush();
+        
+        $messages = $this->mongoDBService->getMessagesBetweenUsers($this->testUserId1, $this->testUserId2);
+        
+        $this->assertCount(2, $messages, 'Should retrieve exactly 2 messages between the users');
+        
+        $foundMessages = [
+            'Message 1 from user1 to user2' => false,
+            'Message 2 from user2 to user1' => false
+        ];
+        
+        foreach ($messages as $message) {
+            $this->assertContains($message->getMessage(), array_keys($foundMessages), 'Retrieved unexpected message');
+            $foundMessages[$message->getMessage()] = true;
+        }
+        
+        foreach ($foundMessages as $messageText => $found) {
+            $this->assertTrue($found, "Message '$messageText' was not retrieved");
+        }
+    }
+    
+    public function testGetUnreadMessageCount(): void
+    {
+        $message1 = new ChatMessage();
+        $message1->setFromUserId($this->testUserId1)
+            ->setToUserId($this->testUserId2)
+            ->setMessage('Unread message 1')
+            ->setSeen(false);
+        
+        $message2 = new ChatMessage();
+        $message2->setFromUserId($this->testUserId1)
+            ->setToUserId($this->testUserId2)
+            ->setMessage('Unread message 2')
+            ->setSeen(false);
+            
+        $message3 = new ChatMessage();
+        $message3->setFromUserId($this->testUserId3)
+            ->setToUserId($this->testUserId2)
+            ->setMessage('Unread message from user3')
+            ->setSeen(false);
+            
+        $message4 = new ChatMessage();
+        $message4->setFromUserId($this->testUserId1)
+            ->setToUserId($this->testUserId2)
+            ->setMessage('Read message')
+            ->setSeen(true);
+        
+        $this->documentManager->persist($message1);
+        $this->documentManager->persist($message2);
+        $this->documentManager->persist($message3);
+        $this->documentManager->persist($message4);
+        $this->documentManager->flush();
+        
+        $unreadCounts = $this->mongoDBService->getUnreadMessageCount($this->testUserId2);
+        
+        $this->assertArrayHasKey($this->testUserId1, $unreadCounts, 'Should have unread messages from user1');
+        $this->assertArrayHasKey($this->testUserId3, $unreadCounts, 'Should have unread messages from user3');
+        
+        $this->assertEquals(2, $unreadCounts[$this->testUserId1], 'Should have 2 unread messages from user1');
+        $this->assertEquals(1, $unreadCounts[$this->testUserId3], 'Should have 1 unread message from user3');
+    }
+    
+    public function testGetConversations(): void
+    {
+        $now = new \DateTime();
+        
+        $message1 = new ChatMessage();
+        $message1->setFromUserId($this->testUserId1)
+            ->setToUserId($this->testUserId2)
+            ->setMessage('Message to user2')
+            ->setSeen(false)
+            ->setCreatedAt(clone $now);
+        
+        $message2 = new ChatMessage();
+        $message2->setFromUserId($this->testUserId3)
+            ->setToUserId($this->testUserId1)
+            ->setMessage('Message from user3')
+            ->setSeen(false)
+            ->setCreatedAt((clone $now)->modify('+1 minute'));
+            
+        $message3 = new ChatMessage();
+        $message3->setFromUserId($this->testUserId2)
+            ->setToUserId($this->testUserId1)
+            ->setMessage('Reply from user2')
+            ->setSeen(true)
+            ->setCreatedAt((clone $now)->modify('-1 minute'));
+        
+        $this->documentManager->persist($message1);
+        $this->documentManager->persist($message2);
+        $this->documentManager->persist($message3);
+        $this->documentManager->flush();
+        
+        $conversations = $this->mongoDBService->getConversations($this->testUserId1);
+        
+        $this->assertCount(2, $conversations, 'Should have 2 conversations');
+        
+        $this->assertEquals($this->testUserId3, $conversations[0]['recipientId'], 'First conversation should be with user3 (most recent)');
+        $this->assertEquals($this->testUserId2, $conversations[1]['recipientId'], 'Second conversation should be with user2');
+        
+        $this->assertCount(1, $conversations[0]['messages'], 'Conversation with user3 should have 1 message');
+        $this->assertCount(2, $conversations[1]['messages'], 'Conversation with user2 should have 2 messages');
     }
 }
